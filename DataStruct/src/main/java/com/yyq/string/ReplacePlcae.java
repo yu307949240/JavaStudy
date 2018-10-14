d2c7e811ab345679d8f2e64897be117e82eb920e
yuyouquan <yuyouquan@xiaomi.com>
2018-10-14 14:11:46 +0800

    commit str

---
+++ DataStruct/src/main/java/com/yyq/string/ReplacePlcae.java
@@ -0,0 +1,38 @@
+package com.yyq.string;
+
+/**
+ * 替换空格
+ * Input: "We Are Happy"
+ * Output: "We%20Are%20Happy"
+ *
+ * @author yyq
+ * @link https://github.com/CyC2018/CS-Notes/blob/master/notes/%E5%89%91%E6%8C%87%20offer%20%E9%A2%98%E8%A7%A3.md#5-%E6%9B%BF%E6%8D%A2%E7%A9%BA%E6%A0%BC
+ * @since 2018/10/13
+ */
+public class ReplacePlcae {
+    public static void replcaePlace(StringBuffer str) {
+        int p1 = str.length() - 1;
+        for (int i = 0; i <= p1; i++) {
+            if (str.charAt(i) == ' ') {
+                str.append("  ");
+            }
+        }
+        int p2 = str.length() - 1;
+        while (p1 >= 0 && p2 > p1) {
+            char c = str.charAt(p1--);
+            if (c == ' ') {
+                str.setCharAt(p2--, '%');
+                str.setCharAt(p2--, '2');
+                str.setCharAt(p2--, '0');
+            } else {
+                str.setCharAt(p2--, c);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("we are happy");
+        replcaePlace(str);
+        System.out.println(str);
+    }
+}
---
+++ DataStruct/src/main/java/com/yyq/string/Trie.java
@@ -0,0 +1,80 @@
+package com.yyq.string;
+
+/**
+ * 字典树实现字符串的存储
+ * @author yyq
+ * @since 2018/10/13
+ */
+public class Trie {
+    class TrieNode {
+        int path; // 有多少个word经过
+        int end; // 有多少个word以此节点结束
+        TrieNode[] map;
+
+        public TrieNode() {
+            path = 0;
+            end = 0;
+            map = new TrieNode[26];
+        }
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // 向字典树中插入一个word
+    public void insert(String word) {
+        if (word == null) {
+            return;
+        }
+        TrieNode node = root;
+        int index = 0;
+        char[] chs = word.toCharArray();
+        for (int i = 0; i < chs.length; i++) {
+            index = chs[i] - 'a';
+            if (node.map[index] == null) {
+                node.map[index] = new TrieNode();
+            }
+            node = node.map[index];
+            node.path++;
+        }
+        node.end++;
+    }
+
+    // 查找字典树中是否包含word
+    public boolean contain(String word) {
+        if (word == null) {
+            return false;
+        }
+        TrieNode node = root;
+        int index = 0;
+        char[] chs = word.toCharArray();
+        for (int i = 0; i < chs.length; i++) {
+            index = chs[i] - 'a';
+            if (node.map[index] == null) {
+                return false;
+            }
+            node = node.map[index];
+        }
+        return node.end != 0;
+    }
+
+    // 删除字典树中的word
+    public void delete(String word){
+        if(contain(word)) {
+            TrieNode node = root;
+            int index = 0;
+            char[] chs = word.toCharArray();
+            for (int i = 0; i < chs.length; i++) {
+                index = chs[i] - 'a';
+                if (node.map[index].path-- == 1) {
+                    node.map[index] = null;
+                }
+                node = node.map[index];
+            }
+            node.end--;
+        }
+    }
+}
